722788ca0cb7631e74c98d673240d9c8ee5df1c7
Njanand-xenon <njanand.xenon@gmail.com>
2022-07-31 16:46:33 +0530

    alan ai
    integration of ai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Homepage from "./Pages/Homepage";
 import About from "./Pages/About";
@@ -7,19 +7,30 @@ import CategoryPage from "./Pages/CategoryPage";
 import { Switch, Route } from "react-router-dom";
 import Appbar from "./components/Appbar.js";
 
+import alanBtn from "@alan-ai/alan-sdk-web";
+
 const App = () => {
-	
-	return (
-		<>
-			<Appbar />
-			<Switch>
-				<Route exact path="/" component={Homepage} />
-				<Route path="/category" component={CategoryPage} />
-				<Route path="/about" component={About} />
-				<Route path="/contact" component={Contact} />
-			</Switch>
-		</>
-	);
+  useEffect(() => {
+    alanBtn({
+      key: "fd5b30a910c1767c84158692cca8e02d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "go:back") {
+          // Call the client code that will react to the received command
+        }
+      },
+    });
+  }, []);
+  return (
+    <>
+      <Appbar />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/category" component={CategoryPage} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
